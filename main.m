79716fa506ec4fc32979f9f14ef6c2fdb3587a04
Albert Skog <skog.albert@gmail.com>
2012-04-25 15:14:12 +0200

    byta mellan menyval funkar

--- main.m
+++ main.m
@@ -1,6 +1,7 @@
 function main
 
     screen_size = get(0,'ScreenSize');
+    top = screen_size(4);
 
     gui = figure('Name','Lab Assistant','Position', screen_size);
     
@@ -10,8 +11,8 @@ function main
 
     uicontrol('Style', 'popup',...
            'String', 'Input|Function Generator|Voltage Generator',...
-           'Position', [100 660 200 50],...
-           'Callback', {@in_callback, in_panel}); 
+           'Position', [90 top-110 200 50],...
+           'Callback', {@in_callback, in_panel})
 
        
     % Create output selector
@@ -19,7 +20,7 @@ function main
     
     uicontrol('Style', 'popup',...
            'String', 'Output|Oscilloscope Screen|Multimeter|Transfer Function',...
-           'Position', [500 660 200 50],...
+           'Position', [500 top-110 200 50],...
            'Callback', {@out_callback, out_panel}); 
    
        
@@ -28,34 +29,48 @@ function main
 
     uicontrol('Style', 'popup',...
            'String', 'Export|Image|LaTeX|Dropbox|Facebook',...
-           'Position', [1000 660 200 50],...
+           'Position', [1050 top-110 200 50],...
            'Callback', {@export_callback, export_panel});
 
 end
 
 function in_callback(callback_object, ~, in_panel)
-     val = get(callback_object,'Value');
-     
-     switch (val)
-         case 3
-             in_voltage_generator(in_panel);
-         case 2
-             in_function_generator(in_panel);
-     end
+    remove_children(in_panel);
+
+    val = get(callback_object,'Value');     
+    switch (val)
+        case 3
+            in_voltage_generator(in_panel);
+        case 2
+            in_function_generator(in_panel);
+    end
 end
 
 function out_callback(callback_object, ~, out_panel)
-         val = get(callback_object,'Value');
-         
-         if(val == 2)
-            out_oscilloscope_picture(out_panel);
-         end
+    remove_children(out_panel);
+    
+    val = get(callback_object,'Value'); 
+    if(val == 2)
+        out_oscilloscope_picture(out_panel);
+    end
 end
 
 function export_callback(callback_object, ~, export_panel)
-         val = get(callback_object,'Value');
-         
-         if(val == 3)
+    remove_children(export_panel);
+    
+    val = get(callback_object,'Value');
+	switch (val)
+        case 2
+            export_image(export_panel);
+        case 3
             export_latex(export_panel);
-         end
+	end
+end
+
+%Removes all the children of an object
+function remove_children(object)
+	set( get(object, 'Children') , 'Visible', 'off');
+    %Will not work properly without a tiny pause
+    pause(0.01);
+    delete(get(object, 'Children'));
 end
