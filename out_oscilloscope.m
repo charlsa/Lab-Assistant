7ddba41d4354c94e385b77be0be4e172c05d5c89
Danhe046 <danhe046@gmail.com>
2012-05-04 12:35:18 +0300

    dataöverföringen och plot av bilden lades till

--- out_oscilloscope.m
+++ out_oscilloscope.m
@@ -83,3 +83,61 @@ end
     disp('klar');
  
 end
+
+function datatrans( address )
+clc;
+scope = gpib('ni',0,address);      % define scope object
+scope.InputBufferSize = 10000;    % specify the input buffer size. If the scanned image is not full then one may be required to increase this.
+scope.TimeOut = 5;                % specify sufficient timeout so that read operations do not timeout.            
+fopen(scope);
+
+fprintf(scope, '*CLS');
+fprintf(scope, 'CH1:POSITION?');
+pos1=fscanf(scope) ;			
+fprintf(scope, 'HORIZONTAL:SCALE?'); %time scale		
+tid=fscanf(scope)
+fprintf(scope, 'HORIZONTAL:SCALE?'); %get time a second time because first time is crap sometimes
+tid=fscanf(scope)
+fprintf(scope, 'CH1:VOLTS?'); 	% voltage scale
+amp=fscanf(scope)
+fprintf(scope, 'Data:Source CH1'); 	%channel 1 is chosed
+fprintf(scope, 'Data:Encdg SRPbinary'); 		% Talformatet se vidare manual för oscilloskopet sid 2-69
+fprintf(scope, 'Data:Width 1'); % Antal bytes per datapunkt.
+fprintf(scope, 'Data:Start 1'); % Starta överföringen med punkt 1 på kurvan.
+fprintf(scope, 'Curve?') 		% Överför kurvan
+data = (fread(scope, 2500));	% Läs överförd data
+
+fprintf(scope, '*CLS');%clear status data
+fprintf(scope, 'CH2:POSITION?');
+pos2=fscanf(scope) ;			%  slask, du behöver enbart skriva fscan(g) här för att väcka inst. ?! Bugg?
+fprintf(scope, 'CH2:VOLTS?'); 	
+amp2=fscanf(scope)
+fprintf(scope, 'Data:Source CH2'); 	% channel 2 is chosed
+fprintf(scope, 'Data:Encdg SRPbinary'); 		% Talformatet se vidare manual för oscilloskopet sid 2-69
+fprintf(scope, 'Data:Width 1'); % Number of bytes per data point.
+fprintf(scope, 'Data:Start 1'); % Start transfer at data point 1 om the curve.
+fprintf(scope, 'Curve?') 		% Överför kurvan
+data2 = (fread(scope, 2500));	% Läs överförd data
+
+gpibclose(scope);
+
+t=str2double(tid(9:15));
+t_vec=(t/250:t/250:t*10);
+
+a=str2double(amp(9:16));
+data=(data-127.5)*a/25;                 %delar med 25=200/8 som är antalet pixlar per ruta (wigertz konstant)
+a2=str2double(amp2(9:16));
+data2=(data2-127.5)*a2/25;
+         
+if data2                            %check if data2 was measured correctly
+    figure=plot(t_vec(15:2500),data2(15:2500)); 
+    hold on
+end
+
+if data
+figure=plot(t_vec(15:2500),data(15:2500));
+end
+
+saveas(figure, 'figur.fig')
+hold off
+end
