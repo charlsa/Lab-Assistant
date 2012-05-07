be627f76154923e3e2a3ea7868a5cec7e7e2b8b1
Kalle <charlsa_1990@hotmail.com>
2012-05-07 12:25:15 +0300

    Update in_frequency_sweep.m

--- in_frequency_sweep.m
+++ in_frequency_sweep.m
@@ -90,39 +90,48 @@ function sweepStart_callback(~,~, parameters, gui)
 address = str2num(get(parameters.gpibAddress,'string'));
 gpibObjF = gpib('ni', 0, address);  % function generator
 
-  [multimeterConnect gpibObjM] = connect_multimeter(); % Returns a string
+[multimeterConnect gpibObjM] = connect_multimeter(); % Returns a string
  
-  freqgenConnect = connect_freqGenerator(parameters); % Returns a string
+freqgenConnect = connect_freqGenerator(parameters); % Returns a string
   
-  if(strcmp(multimeterConnect, 'OKEJ') && strcmp(freqgenConnect, 'OKEJ'))
-  fopen(gpibObjF);
-  fopen(gpibObjM);
-    
-%%%% Start frequency-loop 
-    start = str2double(get(parameters.starting,'string'));
-   ending = str2double(get(parameters.ending,'string'));
-   length = str2double(get(parameters.steplength,'string'));
+if(strcmp(multimeterConnect, 'OKEJ') && strcmp(freqgenConnect, 'OKEJ'))
+    fopen(gpibObjF);
+    fopen(gpibObjM);
 
-    a = 1;  
+    %%%% Start frequency-loop 
+    start = str2double(get(parameters.starting,'string'));
+    ending = str2double(get(parameters.ending,'string'));
+    length = str2double(get(parameters.steplength,'string'));
+
+    a = 1;
+    time = (ending-start)/length;
+    i = 0;
+    H = waitbar(0);
+
     for n=start:length:ending 
         %Set frequency
-        fprintf(gpibObjF,['FREQ ', num2str(n)]);   
+        fprintf(gpibObjF,['FREQ ', num2str(n)]); 
 
+
+        waitbar(mod(i,1),H,'Please wait while picture is loading!');
+        i = i + 1/time;
         %Take measurment
-        fprintf(gpibObjM,'MEAS:CURR:AC?');
+        fprintf(gpibObjM,'MEAS:VOLT:AC?');
         VOLT(a) = fscanf(gpibObjM,'%e');
         FREQ(a) = n;
         a=a+1;
     end
-    
+
+    delete(H);
+
     fclose(gpibObjM);
     fclose(gpibObjF);
-    
-    print(VOLT, FREQ, gui);
-    
-  else
-     fclose(instrfind);
-  end
+
+    print(VOLT, FREQ);
+
+else
+    fclose(instrfind);
+end
   
 end
 
@@ -131,43 +140,41 @@ function out = connect_freqGenerator(parameters)
 try
     address = str2num(get(parameters.gpibAddress,'string'));
     gpibObj = gpib('ni', 0, address);
-    
+
     fopen(gpibObj);
     fprintf(gpibObj, '*CLS');
-    set(parameters.gpibConnection,'string' ,'GPIB Connected');
     set(parameters.gpibAddress,'BackgroundColor' ,'white');
     pause(0.01);
-    
+
     choice = get(parameters.wave,'Value'); 
-     
+
     switch (choice)
-         case 1
-             waveform = 'nothing';
-         case 2
-             waveform = 'FUNC SIN';
-         case 3
-             waveform = 'FUNC SQU';
-         case 4
-             waveform = 'FUNC TRI';
-         case 5
-             waveform = 'FUNC RAMP';
+        case 1
+        waveform = 'nothing';
+        case 2
+        waveform = 'FUNC SIN';
+        case 3
+        waveform = 'FUNC SQU';
+        case 4
+        waveform = 'FUNC TRI';
+        case 5
+        waveform = 'FUNC RAMP';
     end
-     
-     fprintf(gpibObj, waveform);
-     
-     Amp = ['VOLT ' get(parameters.amplitud,'string')]; % Volt = 'VOLT value', from int to string 
-     
-     fprintf(gpibObj, Amp);
-     fclose(gpibObj);
-     out = 'OKEJ';
-    
+
+    fprintf(gpibObj, waveform);
+
+    Amp = ['VOLT ' get(parameters.amplitud,'string')]; % Volt = 'VOLT value', from int to string 
+
+    fprintf(gpibObj, Amp);
+    fclose(gpibObj);
+    out = 'OKEJ';
+
 catch err
     msgbox(err.getReport, 'Frequency Generator!', 'warn');
-    set(parameters.gpibConnection,'string' ,'GPIB Disconnected');
     set(parameters.gpibAddress,'BackgroundColor' ,'red');
     out = 'ERROR';
     fclose(instrfind);
-    disp(err); % fel sök
+    disp(err); % fel sÃ¶k
 end
 
 end
@@ -193,28 +200,28 @@ catch err
     set(multimeter.gpibAddress,'BackgroundColor' ,'red');
     out = 'ERROR';
     fclose(instrfind);
-    disp(err); % fel sök
+    disp(err); % fel sÃ¶k
 end
 
 end
 
-function print(volt, frequency, gui)
+function print(volt, frequency)
     
-    semilogx(frequency, db(volt));
-    xlabel('Frequency [HZ]');
-    ylabel('Voltage [dB]');
-    grid on;
-    
-    % save figure
-    tmp_figure = figure(10);
-    semilogx(frequency, db(volt));
-    xlabel('Frequency [HZ]');
-    ylabel('Voltage [dB]');
-    grid on;
-    saveas(tmp_figure, 'figure', 'fig');
-    close(tmp_figure);
-    
-    % save data 
-    save 'data.mat' 'frequency' 'volt';
+semilogx(frequency, db(volt));
+xlabel('Frequency [HZ]');
+ylabel('Voltage [dB]');
+grid on;
+
+% save figure
+tmp_figure = figure(10);
+semilogx(frequency, db(volt));
+xlabel('Frequency [HZ]');
+ylabel('Voltage [dB]');
+grid on;
+saveas(tmp_figure, 'figure', 'fig');
+close(tmp_figure);
+
+% save data 
+save 'data.mat' 'frequency' 'volt';
     
 end
