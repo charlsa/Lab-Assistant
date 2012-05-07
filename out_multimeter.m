90a2b6c85dfb685d31b2b14390afc1d4d9ce5e2b
Kalle <charlsa_1990@hotmail.com>
2012-05-07 12:18:06 +0300

    Klar med display

--- out_multimeter.m
+++ out_multimeter.m
@@ -3,45 +3,46 @@ function out_multimeter(out_panel)
     takeValue = false;
         
     multi.measur = uicontrol('Style', 'popup',... 
-        'String', 'Measurement|Voltage [DC]|Voltage [AC]|Current[DC]|Current[AC]',...
-        'parent', out_panel,...
-        'Position', [50 850 100 20]); 
+            'String', 'Measurement|Voltage [DC]|Voltage [AC]|Current[DC]|Current[AC]',...
+            'parent', out_panel,...
+            'Position', [160 600 100 20]); 
 
     uicontrol('Style', 'text',...
             'parent', out_panel,...
             'string', 'GPIB address:',...
-            'position', [50 800 100 20]);
+            'horizontalalign', 'right',...
+            'BackgroundColor', 'white',...
+            'position', [30 598 70 20]);
 
     multi.gpibAddress =  uicontrol('Style', 'edit',...
-                'parent', out_panel,...
-                'string', '0',...
-                'BackgroundColor', 'white',...
-                'position', [150 800 50 25]);
+            'parent', out_panel,...
+            'string', '0',...
+            'BackgroundColor', 'white',...
+            'position', [100 600 40 20]);
     
     multi.display = uicontrol('Style', 'text',...
             'parent', out_panel,...
-            'string', 'Value',...
-            'position', [250 800 100 20]);    
-   
-    multi.unit = uicontrol('Style', 'text',...
-            'parent', out_panel,...
-            'string', 'Value',...
-            'position', [300 800 100 20]); 
+            'string', '...',...
+            'BackgroundColor', 'black',...
+            'ForegroundColor', [0 1 0.5],...
+            'fontname', 'BankGothic Md BT',...
+            'fontsize', 23,...
+            'position', [30 300 415 40]);    
         
     multi.stop = uicontrol('Style', 'pushbutton',...
-        'parent', out_panel,...
-        'string', 'Stop',...
-        'position', [200 750 100 20],...
-        'callback', {@multimeter_stop, multi});
-    
+            'parent', out_panel,...
+            'string', 'Stop',...
+            'position', [380 600 75 20],...
+            'callback', {@multimeter_stop, multi});
+
     set(multi.stop, 'Enable', 'off');
-        
+
     multi.start = uicontrol('Style', 'pushbutton',...
-        'parent', out_panel,...
-        'string', 'Start',...
-        'position', [50 750 100 20],...
-        'callback', {@multimeter_callback, multi});
-        
+            'parent', out_panel,...
+            'string', 'Start',...
+            'position', [285 600 75 20],...
+            'callback', {@multimeter_callback, multi});
+
 end
 
 function multimeter_stop(~,~,multi)
@@ -64,53 +65,56 @@ try
     
     fopen(gpibObj);
     fprintf(gpibObj, '*CLS');
-    set(multi.gpibConnection,'string' ,'GPIB Connected');
     set(multi.gpibAddress,'BackgroundColor' ,'white');
     
-switch (get(multi.measur,'Value'))
-    case 1
-        disp('set measuring');
-    case 2 % Volt DC
-        voltORcurrent = 'MEAS:VOLT:DC?';
-        fprintf(gpibObj,'CONF:VOLT:DC');
-        set(multi.unit,'string','V');
-       
-    case 3 % Volt AC
-        voltORcurrent = 'MEAS:VOLT:AC?';
-        fprintf(gpibObj,'CONF:VOLT:AC');
-        set(multi.unit,'string','V');
-         
-    case 4 % Current DC
-        voltORcurrent = 'MEAS:CURR:DC?';
-        fprintf(gpibObj,'CONF:CURR:DC'); 
-        set(multi.unit,'string','A');
+        switch (get(multi.measur,'Value'))
+        case 1
+            disp('set measuring');
+            
+        case 2 % Volt DC
+            voltORcurrent = 'MEAS:VOLT:DC?';
+            fprintf(gpibObj,'CONF:VOLT:DC');
+            unit = ' V';
+           
+        case 3 % Volt AC
+            voltORcurrent = 'MEAS:VOLT:AC?';
+            fprintf(gpibObj,'CONF:VOLT:AC');
+            unit = ' V';
+          
+        case 4 % Current DC
+            voltORcurrent = 'MEAS:CURR:DC?';
+            fprintf(gpibObj,'CONF:CURR:DC'); 
+            unit = ' A';
+          
+        case 5 % Current AC
+            voltORcurrent = 'MEAS:CURR:AC?';
+            fprintf(gpibObj,'CONF:CURR:AC');
+            unit = ' A';
+    
+        end
         
-    case 5 % Current AC
-        voltORcurrent = 'MEAS:CURR:AC?';
-        fprintf(gpibObj,'CONF:CURR:AC'); 
-        set(multi.unit,'string','A');
-        
-end
-fclose(gpibObj);
-set(outMenu, 'Enable', 'off');
-set(multi.measur, 'Enable', 'off');
-set(multi.stop, 'Enable', 'on');
-pause(0.5);
-
-while takeValue
-    fopen(gpibObj);
-    pause(0.01);
-    fprintf(gpibObj,voltORcurrent);
-    set(multi.display,'string',fscanf(gpibObj));
-    pause(0.5);
     fclose(gpibObj);
-end
+    set(outMenu, 'Enable', 'off');
+    set(multi.measur, 'Enable', 'off');
+    set(multi.stop, 'Enable', 'on');
+    pause(0.5);
+    set(multi.display,'BackgroundColor', 'black');
+
+    while takeValue
+        fopen(gpibObj);
+        pause(0.01);
+        fprintf(gpibObj,voltORcurrent);
+        temp = fscanf(gpibObj);
+        set(multi.display,'string',[temp(1:15), unit]);
+        pause(0.5);
+        fclose(gpibObj);
+    end
    
-catch err
-    msgbox(err.getReport, 'Multimeter!', 'warn');
-    set(multi.gpibAddress,'BackgroundColor' ,'red');
-    fclose(instrfind);
-    disp(err); % fel sök
-end
+    catch err
+        msgbox(err.getReport, 'Multimeter!', 'warn');
+        set(multi.gpibAddress,'BackgroundColor' ,'red');
+        fclose(instrfind);
+        disp(err);
+    end
 
 end
